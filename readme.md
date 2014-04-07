50857bd9a240f77eff8fbfbbe7700e27a8958c2a
James Conkling <james.lane.conkling@gmail.com>
2014-04-07 12:29:18 -0400

    init repo

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Project Title
+
+Description [project name](http://www.com/).
+
+## Data Sources
+
+- Dam data collected and published by [International Rivers](http://www.internationalrivers.org/)
+- Elevation data from [CGIAR](http://srtm.csi.cgiar.org/): SRTM 90m Digital Elevation Data
+- River shapefiles from the USGS and [WWF HydroSHEDS](http://hydrosheds.cr.usgs.gov/dataavail.php)
+- Administrative boundary and physical geography shapefiles from [Natural Earth](http://www.naturalearthdata.com/downloads/)
---
+++ script.js
@@ -0,0 +1,65 @@
+// Syria Conflict Mapping
+// "Making Sense of Syria": The Special Project Unit
+
+var mapLayers = {};
+
+var map = L.mapbox.map('map', 'helsinki.hnhinfii', {
+    minZoom: 7,
+    maxZoom: 16,
+    maxBounds: [[30.8,35.1],    //[[min-y, min-x],
+              [38.1,42.8]]   //[max-y, max-x]],
+})
+    .setView([34.5, 37.5], 8);
+
+map.addControl(L.mapbox.shareControl().setPosition('topright'));
+map.zoomControl.setPosition('topright');
+
+
+// Add custom legend
+// var mapLegend = L.mapbox.legendControl({ position:'bottomright' }).addLegend(
+//     document.getElementById('legend-content').innerHTML);
+// map.addControl(mapLegend);
+
+$('.arrow').on('click', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    var $this = $(this),
+        $contentContainer = $('.content-container');
+
+    if($this.hasClass('active')) {
+        $this.removeClass('active');
+        $contentContainer.removeClass('active');
+    } else {
+        $this.addClass('active');
+        $contentContainer.addClass('active').find('.calendar a:first').trigger('click');
+    }
+});
+
+
+
+$('.calendar').on('click', 'a', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    var $this = $(this),
+        mapId = $this.data('id');
+
+    for (var mapLayer in mapLayers){
+        map.removeLayer(mapLayers[mapLayer]);
+    }
+
+    if (! mapLayers[mapId]){
+        mapLayers[mapId] = L.mapbox.tileLayer('helsinki.' + mapId);
+    }
+
+    map.addLayer(mapLayers[mapId]);
+
+    $('.calendar a.active').removeClass('active');
+    $this.addClass('active');
+    // if (map.hasLayer(layer)){
+    //     map.removeLayer(layer);
+    // } else {
+    //     map.addLayer(layer);
+    // }
+});
